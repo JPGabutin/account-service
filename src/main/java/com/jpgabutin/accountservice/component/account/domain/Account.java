2e338fe246ef090cc8cb730b32bcc3f5395c3df3
jpgab <gabutin.jp@gmail.com>
2026-05-19 22:53:58 +0800

    feature: add account entity

---
+++ src/main/java/com/jpgabutin/accountservice/component/account/domain/Account.java
@@ -0,0 +1,84 @@
+package com.jpgabutin.accountservice.component.account.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String customerName;
+
+    @Column(nullable = false, length = 20)
+    private String customerMobile;
+
+    @Column(nullable = false, length = 50)
+    private String customerEmail;
+
+    @Column(nullable = false, length = 100)
+    private String address1;
+
+    @Column(length = 100)
+    private String address2;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountType accountType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerMobile() {
+        return customerMobile;
+    }
+
+    public void setCustomerMobile(String customerMobile) {
+        this.customerMobile = customerMobile;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+}
---
+++ src/main/java/com/jpgabutin/accountservice/component/account/domain/AccountType.java
@@ -0,0 +1,6 @@
+package com.jpgabutin.accountservice.component.account.domain;
+
+public enum AccountType {
+    S,
+    C
+}
